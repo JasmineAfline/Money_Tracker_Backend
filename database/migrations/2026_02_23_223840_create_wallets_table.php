9d59aeada4d7b1e4da55173a7fbfb2331401a0ee
Afline Jasmine <afline47@gmail.com>
2026-02-24 21:19:35 +0300

    Refined user and wallet migrations

--- database/migrations/2026_02_23_223840_create_wallets_table.php
+++ database/migrations/2026_02_23_223840_create_wallets_table.php
@@ -4,15 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration // Updated to anonymous class
+return new class extends Migration 
 {
     public function up(): void
     {
         Schema::create('wallets', function (Blueprint $table) {
             $table->id();
-            // This links the wallet to a specific user
+          
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('name'); // e.g., "M-Pesa", "Bank", "Cash"
+            $table->string('name'); 
             $table->decimal('balance', 15, 2)->default(0.00);
             $table->timestamps();
         });
